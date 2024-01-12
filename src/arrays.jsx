9aaf489b4855258a1ac2ad7f388b24dce761eb3f
Ankita <ankitasinghr45@gmail.com>
2024-01-12 21:34:56 +0530

    new changes

---
+++ src/arrays.jsx
@@ -0,0 +1,34 @@
+const animeList =[
+    {
+        name:"Spy X Family",
+        link:"https://www.netflix.com/title/81511410",
+        imgSource:"https://images.justwatch.com/poster/308633830/s332/season-2"
+    },
+    {
+        name:"Demon Slayer",
+        link:"https://www.netflix.com/title/81091393",
+        imgSource:"https://images.justwatch.com/poster/258527365/s718/season-1.jpg"
+    },
+    {
+        name:"Blue Lock",
+        link:"https://www.netflix.com/title/81640753",
+        imgSource:"https://m.media-amazon.com/images/S/pv-target-images/1c7673199fafc72304080c0811a5ff1b7bb18bb59631ec51dca09853e2344629.jpg"
+    },
+    {
+        name:"CyberPunk",
+        link:"https://www.netflix.com/title/81054853",
+        imgSource:"https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p22812509_b_v13_ab.jpg"
+    },
+    {
+        name:"Jujutsu Kaisen",
+        link:"https://www.netflix.com/title/81278456",
+        imgSource:"https://m.media-amazon.com/images/M/MV5BMTMwMDM4N2EtOTJiYy00OTQ0LThlZDYtYWUwOWFlY2IxZGVjXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg"
+    },
+    {
+        name:"Kotaro Lives Alone",
+        link:"https://www.netflix.com/title/81275353",
+        imgSource:"https://images.hive.blog/0x0/https://files.peakd.com/file/peakd-hive/tegoshei/243g75t6RTddMkYmNtzhNiHFPCy1yeaeaNEr3khkW5iXC5NfJnDEnfQNrD5Qz9L3dFstk.png"
+    }
+]
+
+export default animeList;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Cards from './Cards';
+import arraydata from './arrays'
 
 ReactDOM.render(
-<>
-<h1 className='heading_style'>NetFlix Top Animes</h1>
+    <>
+        <h1 className='heading_style'>NetFlix Top Animes</h1>
+        {arraydata.map(data => {
+            return (
+                <Cards imgSource={data.imgSource}
+                    name={data.name} link={data.link} />
+            )
+        })}
 
-<Cards imgSource="https://images.justwatch.com/poster/308633830/s332/season-2"
-name = "Spy X Family" link="https://www.netflix.com/title/81511410" />
-
-<Cards imgSource="https://images.justwatch.com/poster/258527365/s718/season-1.jpg"
-name = "Demon Slayer" link="https://www.netflix.com/title/81091393" />
-
-<Cards imgSource="https://m.media-amazon.com/images/S/pv-target-images/1c7673199fafc72304080c0811a5ff1b7bb18bb59631ec51dca09853e2344629.jpg"
-name = "Blue Lock" link="https://www.netflix.com/title/81640753"/>
-
-<Cards imgSource="https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p22812509_b_v13_ab.jpg"
-name = "Cyber Punk" link="https://www.netflix.com/title/81054853" />
-
-<Cards imgSource="https://m.media-amazon.com/images/M/MV5BMTMwMDM4N2EtOTJiYy00OTQ0LThlZDYtYWUwOWFlY2IxZGVjXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg"
-name = "Jujutsu Kaisen" link="https://www.netflix.com/title/81278456" />
-
-</>,document.getElementById('root'));
+    </>, document.getElementById('root'));
 
